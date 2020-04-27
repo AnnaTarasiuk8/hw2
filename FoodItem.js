e78dcf040a174bf02d3ae93f4b3c2c8e9da91d7b
Anna Tarasiuk <49215581+AnnaTarasiuk8@users.noreply.github.com>
2020-04-26 17:09:05 -0700

    Add files via upload

---
+++ FoodItem.js
@@ -0,0 +1,5 @@
+function Food(pName,pCalories,pQuantity){
+    this.pName = pName;
+    this.pCalories = pCalories;
+    this.pQuantity = pQuantity;
+}
---
+++ HW2.js
@@ -0,0 +1,45 @@
+let array = Array();
+
+
+function addYourTotal() {
+    let name = document.getElementById("nameOfFood");
+    let calories = document.getElementById("calories");
+    let quantity = document.getElementById("quantity");
+    let result = document.getElementById("result");
+    let ul = document.getElementById("listOfFood");
+
+    if (name.value === "" || calories.value === "" || quantity.value === "" || isNaN(parseInt(calories.value))) {
+        result.innerText = "злые слова негодования";
+        return;
+    }
+    array.push(new Food(name.value, calories.value, quantity.value));
+
+    //тут через дом моддель создаешь и добавляешь Ли
+    let li = document.createElement("li");
+    li.innerText = name.value + " ," + calories.value + " ," + quantity.value;
+    ul.append(li);
+
+    name.value = "";
+    calories.value = "";
+    quantity.value = "";
+    count();
+}
+
+function count() {
+
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += parseInt(array[i].pCalories);
+    }
+    document.getElementById("result").innerText = "вы потолстели сегодня на - " + sum;
+}
+
+function fClear() {
+    document.getElementById("result").innerText = "";
+    let ul = document.getElementById("listOfFood");
+    array = Array();
+
+    for (let i = ul.children.length - 1; i >= 0; i--) {
+        ul.children[i].remove();
+    }
+}
